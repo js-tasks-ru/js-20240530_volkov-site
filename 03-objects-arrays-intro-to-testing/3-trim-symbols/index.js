dc71ba718b5ce0490dd536814a96aa807a03b74b
Aliaksandr Volkau <volkov-site@mail.ru>
2024-06-09 16:11:46 +0400

    #03 function trimSymbols has been implemented

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,15 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) return '';
+  if (!size || string.length === 0) return string;
 
+  let accumulator = 1;
+  return string.split('').map((letter, index, array) => {
+    if (letter !== array[index + 1]) { accumulator = 1; return letter; }
+    if (letter === array[index + 1] && accumulator >= size) return '';
+      
+    accumulator++;
+    return letter;
+  }).join('');
 }
